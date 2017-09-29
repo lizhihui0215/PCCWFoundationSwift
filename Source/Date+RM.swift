4e96f45d3c2a57f0a5690d0f51907637babef6f2
李智慧 <lizhihui0215@gmail.com>
2017-09-29 16:29:21 +0800

    refine code

---
+++ Source/Date+RM.swift
@@ -0,0 +1,17 @@
+//
+//  Date+RM.swift
+//  ResourceManager
+//
+//  Created by 李智慧 on 2017/4/24.
+//  Copyright © 2017年 北京海睿兴业. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    func date(template: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = template
+        return dateFormatter.string(from: self)
+    }
+}
